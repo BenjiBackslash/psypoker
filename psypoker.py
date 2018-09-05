6460531a3672a7160e0edad890b0b65f05e22ab0
Hanan Wiener <hanan888@gmail.com>
2018-09-05 18:57:20 +0300

    print to console

--- psypoker.py
+++ psypoker.py
@@ -34,6 +34,7 @@ if __name__ == "__main__":
         hand, deck = parse_input_line(line)
         best_combination = calc_best_combination(hand, deck)
         result_line = make_result_line(hand, deck, best_combination)
+        print(result_line)
         results.append(result_line)
     with open("res/output", "wt") as fdw:
         fdw.writelines(results)
